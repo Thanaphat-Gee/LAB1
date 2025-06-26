0f3c09f9a554a2e7f7aad247b883fef376bf3648
Thanaphat-Gee <thanaphat.ar@ku.th>
2025-06-26 16:45:52 +0700

    Update RollingBall.java

--- Lib/RollingBall.java
+++ Lib/RollingBall.java
@@ -3,12 +3,30 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class RollingBall extends JPanel{
-
+public class RollingBall extends JPanel implements ActionListener{
+    public int r1 = 0 , x = 100;
+    
+    public RollingBall(){
+        Timer timer = new Timer(100, this);
+        timer.start();
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawOval(100,90,80,80);
+        g.drawOval(x,90,80,80);
         g.setColor(Color.BLACK);
-        g.fillArc(100, 90, 80, 80, 0, 180);
+        g.fillArc(x, 90, 80, 80, r1, 180);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        x -= 2;
+        r1 += 2;
+        if (x == -80) {
+            x = getWidth();
+        }
+        if (r1 == 360) {
+            r1 = 0;
+        }
+        repaint();
+
     }
 }
